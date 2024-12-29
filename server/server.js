3ecb41823f41f420709f95ebb96e40ca419cde13
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-29 15:29:14 +0700

    Set up some .env.local

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const corsOptions = {
     origin: ['http://localhost:5173'],
 }
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.local' });
+const port = process.env.PORT;
 
 app.use(cors(corsOptions));
 
@@ -11,6 +14,6 @@ app.get('/api', (req, res) => {
     res.json({"fruits": ["apple", "banana", "orange"]});
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('Server running on port 8080');
 });
