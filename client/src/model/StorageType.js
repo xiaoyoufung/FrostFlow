cf035afc09bb3f2d85503e06c1df439ced250b80
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 02:10:41 +0700

    Create StorageType.js for ENUM storage
    Create ENUM storage for storage type

---
+++ client/src/model/StorageType.js
@@ -0,0 +1,8 @@
+const StorageStatus = Object.freeze({
+    OUT_OF_STOCK: 'OUT_OF_STOCK',
+    LOW_STOCK: 'LOW_STOCK',
+    WELL_STOCK: 'WELL_STOCK',
+    FULL_STOCK: 'FULL_STOCK'
+  });
+
+export default StorageStatus;
