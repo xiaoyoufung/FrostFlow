cb01fa34947d23178b342619bffeb80c622d18a6
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-02 17:36:51 +0700

    Update NavMobileManu.jsx
    fix styling of the menu

--- client/src/components/navbar/NavMobileMenu.jsx
+++ client/src/components/navbar/NavMobileMenu.jsx
@@ -4,13 +4,14 @@ import PropTypes from "prop-types";
 const NavMobileMenu = ({ menu }) => {
   return (
     <DisclosureButton
-          key={menu.name}
-          as="a"
-          href={menu.href}
-          aria-current={menu.current ? "page" : undefined}
-          className={`block px-5 py-2 text-base font-medium ${menu.current ? "text-indigo-600 border-l-4 border-indigo-500" : "text-gray-500 hover:bg-gray-700 hover:text-white"}`}>
-          {menu.name}
-        </DisclosureButton>
+      key={menu.name}
+      as="a"
+      href={menu.href}
+      aria-current={menu.current ? "page" : undefined}
+      className={`block px-5 py-2 text-base font-medium ${menu.current ? "text-indigo-600 bg-indigo-50 border-l-4 border-indigo-500" : "text-gray-600 hover:bg-gray-50 hover:text-gray-800 hover:border-l-4 hover:border-gray-300"}`}
+    >
+      {menu.name}
+    </DisclosureButton>
   );
 };
 
