0f2810a0acdd63a2ca89144c922e49b83d043e85
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-02 19:12:45 +0700

    Edit Navbar.jsx
    Successfully make the like style

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -40,14 +40,6 @@ function classNames(...classes) {
 export default function Example() {
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
       <div className="min-h-full">
         <Disclosure as="nav" className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -58,25 +50,19 @@ export default function Example() {
                 </div>
               </div>
               {/* middle section */}
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
+              <div className="hidden lg:flex h-full">
+                <div className="ml-10  space-x-4 h-full lg:flex">
                   {navigation.map((item) => (
-                    <div key={item.name} className="relative">
                       <a
+                        key={item.name}
                         href={item.href}
                         aria-current={item.current ? "page" : undefined}
-                        className={classNames(
-                          item.current
-                            ? "text-grey"
-                            : "text-gray-500 hover:bg-white hover:text-black",
-                          "rounded-md px-3 py-2 text-sm font-medium"
-                        )}
+                        className={`content-center px-1 pt-1 text-sm font-medium h-full ${item.current
+                          ? "text-grey"
+                          : "text-gray-500 hover:bg-white hover:text-black border-b-2"}`}
                       >
                         {item.name}
                       </a>
-
-                      <div className={`absolute -bottom-5 left-0 right-0 h-0.5 ${item.current && "bg-gray-900"}`}></div>
-                    </div>
                   ))}
                 </div>
               </div>
