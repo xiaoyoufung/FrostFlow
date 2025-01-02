0872fc3d4aba23376ca538c9b920bc14c818d9ef
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 01:23:21 +0700

    Styling Page.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,9 @@
-import Navbar from "./components/navbar/Navbar";
+import Page from "./components/Page";
 
 const App = () => {
   return (
     <>
-      <div className="min-h-full">
-        <Navbar />
-
-        
-        <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <div className="text-5xl ">App</div>
-          </div>
-        </main>
-      </div>
+      <Page title="Dashboard" description="Monitor every activity of your inventory" />
     </>
   );
 };
