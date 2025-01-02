4aeb6883b01a917e8b39d347321262b6a1dc5be1
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-02 17:20:05 +0700

    Update NavMobileMenu.jsx
    fix styling of the menu

--- client/src/components/navbar/NavMobileMenu.jsx
+++ client/src/components/navbar/NavMobileMenu.jsx
@@ -3,23 +3,14 @@ import PropTypes from "prop-types";
 
 const NavMobileMenu = ({ menu }) => {
   return (
-    <div key={menu.name} className="relative">
-      <div
-        className={`relative h-10 ${menu.current && "bg-indigo-50"} flex items-center`}
-      >
-        <div
-          className={`absolute inset-y-0 left-0 w-1 ${menu.current && "bg-indigo-600"}`}
-        ></div>
-        <DisclosureButton
+    <DisclosureButton
           key={menu.name}
           as="a"
           href={menu.href}
           aria-current={menu.current ? "page" : undefined}
-          className={`block rounded-md px-5 py-2 text-base font-medium ${menu.current ? "text-indigo-600" : "text-gray-500 hover:bg-gray-700 hover:text-white"}`}>
+          className={`block px-5 py-2 text-base font-medium ${menu.current ? "text-indigo-600 border-l-4 border-indigo-500" : "text-gray-500 hover:bg-gray-700 hover:text-white"}`}>
           {menu.name}
         </DisclosureButton>
-      </div>
-    </div>
   );
 };
 
