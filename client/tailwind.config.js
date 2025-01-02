604a9a7ae97df8110c9380a4d59e12084d942b2f
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 04:35:30 +0700

    Customize tailwind color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,14 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      'storage-status': {
+        'OUT_OF_STOCK': '#DC8583',
+        'LOW_STOCK': '#EAA05F',
+        'WELL_STOCK': '#F6C86B',
+        'FULL_STOCK': '#4E9E83',
+      },
+    },
   },
   plugins: [],
 }
