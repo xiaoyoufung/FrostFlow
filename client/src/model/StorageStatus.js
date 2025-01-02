1dbdc24a6b1b0a20921a891a96e222859d574d08
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 03:07:43 +0700

    Edit StorageStatus.js
    add "none" and "transparent" to the Storage status

--- client/src/model/StorageStatus.js
+++ client/src/model/StorageStatus.js
@@ -2,7 +2,9 @@ const StorageStatus = Object.freeze({
     OUT_OF_STOCK: 'OUT_OF_STOCK',
     LOW_STOCK: 'LOW_STOCK',
     WELL_STOCK: 'WELL_STOCK',
-    FULL_STOCK: 'FULL_STOCK'
+    FULL_STOCK: 'FULL_STOCK',
+    NONE: 'NONE',
+    TRANSPARENT: 'TRANSPARENT'
   });
 
 export default StorageStatus;
