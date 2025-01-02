2b6e4e0f03ee6c1ca44f1eb89ad6492318b1a522
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-02 16:41:02 +0700

    create NavMobileMenu.jsx
    seperate mobile menu items into NavMobileMenu.jsx file

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -11,6 +11,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import NavLogo from "./Navlogo";
 import NavbarUserProfile from "./NavbarUserProfile";
+import NavMobileMenu from "./NavMobileMenu";
 
 const user = {
   name: "Tom Cook",
@@ -110,7 +111,7 @@ export default function Example() {
               </div>
               <div className="-mr-2 flex md:hidden">
                 {/* Mobile menu button */}
-                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md  p-2 text-gray-400 hover:bg-gray-100 hover:text-grey focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   <Bars3Icon
@@ -128,24 +129,13 @@ export default function Example() {
 
           <DisclosurePanel className="md:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+
               {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  aria-current={item.current ? "page" : undefined}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                >
-                  {item.name}
-                </DisclosureButton>
+                <NavMobileMenu key={item.name} navigation={item} />
               ))}
+
             </div>
-            <div className="border-t border-gray-700 pb-3 pt-4">
+            <div className="border-t border-gray-200 pb-3 pt-4">
               <NavbarUserProfile {...user} />
               <div className="mt-3 space-y-1 px-2">
                 {userNavigation.map((item) => (
@@ -153,7 +143,7 @@ export default function Example() {
                     key={item.name}
                     as="a"
                     href={item.href}
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-100 hover:text-black"
                   >
                     {item.name}
                   </DisclosureButton>
