0116ad0001f920eeef0d7c3805c20e020ea45507
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 04:36:28 +0700

    Create InventContainer.jsx

---
+++ client/src/components/dashboard/inventory/InventSuggest.jsx
@@ -0,0 +1,15 @@
+import InventContainer from "./InventContainer";
+import TopContainer from "../TopContainer";
+
+const InventSuggest = () => {
+  return (
+    <TopContainer>
+      <div className="px-2 m-2 flex flex-col justify-center">
+        <h1 className="font-semibold py-3 text-base">Inventory Suggestion</h1>
+        <InventContainer />
+      </div>
+    </TopContainer>
+  );
+};
+
+export default InventSuggest;
