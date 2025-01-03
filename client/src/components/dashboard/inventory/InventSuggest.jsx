de8a47f6f59675d6013dc8e44c655def0509c7d3
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 15:56:43 +0700

    Make StatusLabel re-useable

--- client/src/components/dashboard/inventory/InventSuggest.jsx
+++ client/src/components/dashboard/inventory/InventSuggest.jsx
@@ -1,7 +1,7 @@
 import InventContainer from "./InventContainer";
 import TopContainer from "../TopContainer";
-import StatusLabel from "./StatusLabel";
-import Status from "../../../utils/StorageStatus";
+import LabelGroup from "./LabelGroup";
+import StatusLogic from "./StatusLogic";
 
 const InventSuggest = () => {
   return (
@@ -9,20 +9,9 @@ const InventSuggest = () => {
         <h1 className="font-semibold p-2 text-base">Inventory Suggestion</h1>
         <div className="flex flex-row items-end justify-center px-8">
           <InventContainer />
-          <LabelGroup />
+          <LabelGroup statusLists={StatusLogic()} />
         </div>
     </TopContainer>
-  );
-};
-
-const LabelGroup = () => {
-  return (
-    <div className="inline-flex flex-col justify-between space-y-3 pl-5 pb-4">
-      <StatusLabel status={Status.OUT_OF_STOCK} text="Out of Stock" />
-      <StatusLabel status={Status.LOW_STOCK} text="Low Stock" />
-      <StatusLabel status={Status.WELL_STOCK} text="Well Stock" />
-      <StatusLabel status={Status.FULL_STOCK} text="Full Stock" />
-    </div>
   );
 };
 
--- client/src/components/dashboard/inventory/StatusLabel.jsx
+++ client/src/components/dashboard/inventory/StatusLabel.jsx
@@ -1,36 +1,26 @@
 import PropTypes from 'prop-types';
-import Status from '../../../utils/StorageStatus';
-const StatusLabel = ({status, text}) => {
-  let color = '';
 
-  switch (status) {
-    case Status.OUT_OF_STOCK:
-      color = 'bg-red-600/75';
-      break;
-    case Status.LOW_STOCK:
-      color = 'bg-orange-500/75';
-      break;
-    case Status.WELL_STOCK:
-      color = 'bg-amber-400';
-      break;
-    case Status.FULL_STOCK:
-      color = 'bg-emerald-600/75';
-      break;
-    default:
-      color = 'DC8583';
-      break;
+import Shape from '../../../utils/Shape';
+
+const StatusLabel = ({shape, color, text}) => {
+  let roundedStyle = '';
+  if(shape === Shape.CIRCLE){
+    roundedStyle = 'rounded-full';
+  } else{
+    roundedStyle = 'rounded-sm';
   }
 
   return (
     <div className='flex flex-row items-center w-full space-x-1'>
-        <div className={`${color} w-2 h-2 rounded-sm`}></div>
+        <div className={`${color} w-2 h-2 ${roundedStyle}`}></div>
         <p className='text-xs text-gray-500'>{text}</p>
     </div>
   )
 }
 
 StatusLabel.propTypes = {
-  status: PropTypes.oneOf(Object.values(Status)).isRequired,
+  shape: PropTypes.oneOf(Object.values(Shape)).isRequired,
+  color: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
 }
 
