419fd808d3944bad9bb814fd70a01b19133f7f8f
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-04 01:41:03 +0700

    Move ColorLabel.jsx file to template folder

---
+++ client/src/components/template/ColorLabel.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+import Shape from '../../utils/Shape';
+
+const ColorLabel = ({shape, color, text}) => {
+  let roundedStyle = '';
+  if(shape === Shape.CIRCLE){
+    roundedStyle = 'rounded-full';
+  } else{
+    roundedStyle = 'rounded-sm';
+  }
+
+  return (
+    <div className='flex flex-row items-center w-full space-x-1'>
+        <div className={`${color} w-2 h-2 ${roundedStyle}`}></div>
+        <p className='text-xs text-gray-500'>{text}</p>
+    </div>
+  )
+}
+
+ColorLabel.propTypes = {
+  shape: PropTypes.oneOf(Object.values(Shape)).isRequired,
+  color: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+}
+
+export default ColorLabel
