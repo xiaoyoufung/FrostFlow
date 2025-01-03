544299b8c363757dd6d74f7f8d9370778ab298ec
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-03 11:26:58 +0700

    Fix tailwind.config.js
    the beginning of biggest bug TT

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,13 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      'storage-status': {
-        'OUT_OF_STOCK': '#DC8583',
-        'LOW_STOCK': '#EAA05F',
-        'WELL_STOCK': '#F6C86B',
-        'FULL_STOCK': '#4E9E83',
+    extend: {
+      colors: {
+        'storage-status': {
+          'OUT_OF_STOCK': '#DC8583',
+          'LOW_STOCK': '#EAA05F',
+          'WELL_STOCK': '#F6C86B',
+          'FULL_STOCK': '#4E9E83',
+        },
       },
     },
   },
